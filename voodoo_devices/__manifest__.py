534cd95539df483edbc1b1cca962078f0b488b3a
Trevor Blumenau <tblumenau@voodoorobotics.com>
2023-12-06 18:55:51 -0600

    update manifest

--- voodoo_devices/__manifest__.py
+++ voodoo_devices/__manifest__.py
@@ -2,8 +2,9 @@
     'name': 'Voodoo Devices',
     'version': '1.0',
     'category': 'Inventory Management',
-    'summary': 'Add support for Voodoo Robotics cloud display devices',
-    
+    'summary': 'Enhance warehouse efficiency with this Odoo addon from Voodoo Robotics, seamlessly integrating IoT pick-to-light systems for optimized picking, putting, kitting, and more, streamlining operations with real-time guidance and automation.',
+    'author': 'Voodoo Robotics',
+    'website': 'https://voodoorobotics.com/',
 #   Choose which line to uncomment below based on your use of batch or wave picking
 #    'depends': ['stock','stock_sms','queue_job', 'web', 'stock_picking_batch', 'stock_picking_wave'],
 #    'depends': ['stock','stock_sms','queue_job', 'web', 'stock_picking_batch'],
